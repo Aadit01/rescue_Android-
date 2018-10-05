356ab9f15b374ae9c1e8ce1d100aa65656885d4c
Unknown <s522553@nwmissouri.edu>
2018-10-04 20:23:48 -0500

    first page and main page commit!

---
+++ panic/app/src/main/java/com/example/s522553/panic/signinActivity.java
@@ -0,0 +1,20 @@
+package com.example.s522553.panic;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class signinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+    public void nextPage(View v){
+        Intent i= new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+}
