d2ef0b7bfa41a23986fdf1e25a65504736ecb33b
Unknown <s522553@nwmissouri.edu>
2018-10-08 15:24:28 -0500

    New commit

--- panic/app/src/main/java/com/example/s522553/panic/policeMapsActivity.java
+++ panic/app/src/main/java/com/example/s522553/panic/policeMapsActivity.java
@@ -40,11 +40,7 @@ public class policeMapsActivity extends FragmentActivity implements OnMapReadyCa
 
         // Add a marker in Missouri and move the camera
         LatLng maryville = new LatLng(40.350951, -94.882671);
-<<<<<<< HEAD
-        mMap.addMarker(new MarkerOptions().position(maryville).title("Marker in Colden Hall"));
-=======
         mMap.addMarker(new MarkerOptions().position(maryville).title("Marker in Maryville"));
->>>>>>> a5e7a8aea85c2f09e54e30f97aaff52873b77f50
         mMap.moveCamera(CameraUpdateFactory.newLatLng(maryville));
     }
 }
