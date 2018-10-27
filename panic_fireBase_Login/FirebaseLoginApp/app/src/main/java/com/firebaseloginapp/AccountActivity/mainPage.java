84d6aa4a6a4b9f541271359f0820122a7d7175d7
Unknown <s522553@nwmissouri.edu>
2018-10-26 23:23:15 -0500

    Firebase Login Authentication
    Made a seperate file to avoid conflict with google API

---
+++ panic_fireBase_Login/FirebaseLoginApp/app/src/main/java/com/firebaseloginapp/AccountActivity/mainPage.java
@@ -0,0 +1,18 @@
+package com.firebaseloginapp.AccountActivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.firebaseloginapp.R;
+
+public class mainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+
+}
